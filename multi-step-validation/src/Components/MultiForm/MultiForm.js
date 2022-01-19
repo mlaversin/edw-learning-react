1249d85927094c22d17703f5c6b741f9def2e083
Morgan Laversin <morganlaversin@gmail.com>
2022-01-19 18:34:35 +0100

    allergies form

--- multi-step-validation/src/Components/MultiForm/MultiForm.js
+++ multi-step-validation/src/Components/MultiForm/MultiForm.js
@@ -5,6 +5,8 @@ import CardBegin from './Infos/CardBegin'
 import CardEnd from './Infos/CardEnd'
 import DietForm from './SubForms/DietForm'
 import FoodStyle from './SubForms/FoodStyle'
+import Allergies from './SubForms/Allergies'
+
 
 export default function MultiForm() {
 
@@ -35,6 +37,7 @@ export default function MultiForm() {
             {formIndex === 1 ? <CardBegin modifyIndex={modifyIndex} />
             : formIndex === 2 ? <DietForm modifyIndex={modifyIndex} />
             : formIndex === 3 ? <FoodStyle modifyIndex={modifyIndex} />
+            : formIndex === 4 ? <Allergies modifyIndex={modifyIndex} />
             : ""
         }
         </div>
