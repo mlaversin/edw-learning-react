756de050308ce43111f71e98436a60beaaa16277
Morgan Laversin <morganlaversin@gmail.com>
2022-01-19 18:30:23 +0100

    food styles form

--- multi-step-validation/src/Components/MultiForm/MultiForm.js
+++ multi-step-validation/src/Components/MultiForm/MultiForm.js
@@ -4,6 +4,7 @@ import Indicator from './Indicator/Indicator'
 import CardBegin from './Infos/CardBegin'
 import CardEnd from './Infos/CardEnd'
 import DietForm from './SubForms/DietForm'
+import FoodStyle from './SubForms/FoodStyle'
 
 export default function MultiForm() {
 
@@ -33,6 +34,7 @@ export default function MultiForm() {
             <Indicator />
             {formIndex === 1 ? <CardBegin modifyIndex={modifyIndex} />
             : formIndex === 2 ? <DietForm modifyIndex={modifyIndex} />
+            : formIndex === 3 ? <FoodStyle modifyIndex={modifyIndex} />
             : ""
         }
         </div>
