05ddb0cd7571e98593f40cf5bf0e2c1b827f37cf
Morgan Laversin <morganlaversin@gmail.com>
2022-01-20 09:58:48 +0100

    hate / love form

--- multi-step-validation/src/Components/MultiForm/MultiForm.js
+++ multi-step-validation/src/Components/MultiForm/MultiForm.js
@@ -1,45 +1,50 @@
-import {useState} from 'react'
-import './MultiForm.css'
-import Indicator from './Indicator/Indicator'
-import CardBegin from './Infos/CardBegin'
-import CardEnd from './Infos/CardEnd'
-import DietForm from './SubForms/DietForm'
-import FoodStyle from './SubForms/FoodStyle'
-import Allergies from './SubForms/Allergies'
-
+import { useState } from 'react';
+import './MultiForm.css';
+import Indicator from './Indicator/Indicator';
+import CardBegin from './Infos/CardBegin';
+import CardEnd from './Infos/CardEnd';
+import DietForm from './SubForms/DietForm';
+import FoodStyle from './SubForms/FoodStyle';
+import Allergies from './SubForms/Allergies';
+import HateLove from './SubForms/HateLove';
 
 export default function MultiForm() {
+  const [formIndex, setFormIndex] = useState(1);
+  const [allFormData, setAllFormData] = useState({
+    dietForm: '',
+    foodStyle: [],
+    allergies: [],
+    prefs: {},
+  });
 
-    const [formIndex, setFormIndex] = useState(1);
-    const [allFormData, setAllFormData] = useState({
-        dietForm: "",
-        foodStyle: [],
-        allergies: [],
-        prefs: {}
-    })
+  const modifyIndex = (index, data) => {
+    setFormIndex(index);
 
-    const modifyIndex = (index, data) => {
-        setFormIndex(index)
+    if (data) {
+      const newData = { ...allFormData };
+      const firstPropNewData = Object.keys(data)[0];
 
-        if(data){
-
-            const newData = {...allFormData}
-            const firstPropNewData = Object.keys(data)[0]
-
-            newData[firstPropNewData] = data[firstPropNewData]
-            setAllFormData(newData)
-        }
+      newData[firstPropNewData] = data[firstPropNewData];
+      setAllFormData(newData);
     }
+  };
 
-    return (
-        <div className='container-multiform'>
-            <Indicator />
-            {formIndex === 1 ? <CardBegin modifyIndex={modifyIndex} />
-            : formIndex === 2 ? <DietForm modifyIndex={modifyIndex} />
-            : formIndex === 3 ? <FoodStyle modifyIndex={modifyIndex} />
-            : formIndex === 4 ? <Allergies modifyIndex={modifyIndex} />
-            : ""
-        }
-        </div>
-    )
+  return (
+    <div className="container-multiform">
+      <Indicator />
+      {formIndex === 1 ? (
+        <CardBegin modifyIndex={modifyIndex} />
+      ) : formIndex === 2 ? (
+        <DietForm modifyIndex={modifyIndex} />
+      ) : formIndex === 3 ? (
+        <FoodStyle modifyIndex={modifyIndex} />
+      ) : formIndex === 4 ? (
+        <Allergies modifyIndex={modifyIndex} />
+      ) : formIndex === 5 ? (
+        <HateLove modifyIndex={modifyIndex} />
+      ) : (
+        ''
+      )}
+    </div>
+  );
 }
