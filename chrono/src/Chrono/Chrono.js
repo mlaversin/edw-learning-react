c267bb57883eba1b683263bbad3f86d4031ebba1
Morgan Laversin <morganlaversin@gmail.com>
2022-01-25 11:26:28 +0100

    chrono functionnality

--- chrono/src/Chrono/Chrono.js
+++ chrono/src/Chrono/Chrono.js
@@ -5,6 +5,7 @@ import PlayImg from '../Images/play.svg'
 import ResetImg from '../Images/reset.svg'
 
 export default function Chrono() {
+  
   const [sessionTime, setSessionTime] = useState(1500)
   const [sessionTimeFixed, setSessionTimeFixed] = useState(1500)
 
@@ -12,31 +13,127 @@ export default function Chrono() {
   const [breakTimeFixed, setBreakTimeFixed] = useState(300)
 
   const [workingChrono, setWorkingChrono] = useState(false)
+  const [state, dispatch] = useReducer(reducer)
+
+  function reducer(state, action) {
+    // eslint-disable-next-line default-case
+    switch (action.type) {
+      case 'TICK':
+        if (sessionTime >= 0) {
+          setSessionTime(sessionTime - 1)
+        } else if (breakTime >= 1) {
+          setBreakTime(breakTime - 1)
+        } else if (breakTime <= 0 && breakTime <= 0) {
+          setSessionTime(sessionTimeFixed)
+          setBreakTime(breakTimeFixed)
+        }
+    }
+  }
+
+  useEffect(() => {
+    let id
+    if (workingChrono) {
+      id = window.setInterval(() => {
+        dispatch({ type: 'TICK' })
+      }, 1000)
+    }
+    return () => {
+      window.clearInterval(id)
+    }
+  }, [workingChrono])
+
+  const playPause = () => {
+    setWorkingChrono(!workingChrono)
+  }
+
+  const handleSession = (e) => {
+    const el = e.target
+
+    if (el.classList.contains('minus')) {
+      if (sessionTime / 60 > 1) {
+        setSessionTime(sessionTime - 60)
+        setSessionTimeFixed(sessionTimeFixed - 60)
+      }
+    } else if (el.classList.contains('plus')) {
+      setSessionTime(sessionTime + 60)
+      setSessionTimeFixed(sessionTimeFixed + 60)
+    }
+  }
+
+  const handleBreak = (e) => {
+    const el = e.target
+
+    if (el.classList.contains('minus')) {
+      if (breakTime / 60 > 1) {
+        setBreakTime(breakTime - 60)
+        setBreakTimeFixed(breakTimeFixed - 60)
+      }
+    } else if (el.classList.contains('plus')) {
+      setBreakTime(breakTime + 60)
+      setBreakTimeFixed(breakTimeFixed + 60)
+    }
+  }
+
+  const resetFunc = () => {
+    if (workingChrono) {
+      setWorkingChrono(!workingChrono)
+    }
+    setSessionTime(sessionTimeFixed)
+    setBreakTime(breakTimeFixed)
+  }
 
   return (
     <div className="container-chrono">
       <div className="container-config">
         <div className="box-btns session">
-          <button className="minus">-</button>
+          <button className="minus" onClick={handleSession}>
+            -
+          </button>
           <span>{sessionTimeFixed / 60}</span>
-          <button className="plus">+</button>
+          <button className="plus" onClick={handleSession}>
+            +
+          </button>
         </div>
+
         <div className="box-btns break">
-          <button className="minus">-</button>
-          <span>{breakTimeFixed / 60 }</span>
-          <button className="plus">+</button>
+          <button className="minus" onClick={handleBreak}>
+            -
+          </button>
+          <span>{breakTimeFixed / 60}</span>
+          <button className="plus" onClick={handleBreak}>
+            +
+          </button>
         </div>
       </div>
 
-      <h1><span>CHRONO</span></h1>
+      <h1>
+        {sessionTime >= 0 ? (
+          <span>
+            {`${Math.trunc(sessionTime / 60)} : ${
+              sessionTime % 60 < 10
+                ? `0${sessionTime % 60}`
+                : `${sessionTime % 60}`
+            }`}
+          </span>
+        ) : (
+          <span>
+            {`${Math.trunc(breakTime / 60)} : ${
+              breakTime % 60 < 10 ? `0${breakTime % 60}` : `${breakTime % 60}`
+            }`}
+          </span>
+        )}
+      </h1>
 
       <div className="container-controllers">
-        <buttons>
-          <img src={workingChrono ? PauseImg : PlayImg} alt='bouton play pause'/>
-        </buttons>
-        <buttons>
-          <img src={ResetImg} alt='bouton reset'/>
-        </buttons>
+        <button onClick={playPause}>
+          <img
+            src={workingChrono ? PauseImg : PlayImg}
+            alt="bouton play pause"
+          />
+        </button>
+        <button onClick={resetFunc}>
+          <img src={ResetImg} alt="bouton reset" />
+        </button>
       </div>
     </div>
   )
